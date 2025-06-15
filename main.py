003aec5877f27b613ef44862853634c2d64f9dc9
LevoArgo <leventaltuntas@hotmail.com>
2025-06-15 18:43:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+# Sabit bot bilgileri
+TELEGRAM_TOKEN = "7964205504:AAEWHnCAQN8kSWQMlS2ocrjOFm3AL2a_l2M"
+CHAT_ID = "5891187255"
+
+@app.route("/hook", methods=["POST"])
+def hook():
+    data = request.get_json()
+
+    # TradingView'den gelen mesaj varsa al, yoksa varsayılan mesaj yolla
+    incoming_msg = data.get("message", "📣 SFP sinyali tetiklendi kralım!")
+
+    # Telegram'a POST isteği gönder
+    response = requests.post(
+        f"https://api.telegram.org/bot7964205504:AAEWHnCAQN8kSWQMlS2ocrjOFm3AL2a_l2M/sendMessage",
+        data={"chat_id": "5891187255", "text": incoming_msg}
+    )
+
+    # Log için istersen bu satırı da bırakabilirsin:
+    print("Telegram yanıtı:", response.text)
+
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run()
